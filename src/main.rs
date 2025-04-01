0db21ca51c79b8a79929795d1268ed038be1a326
Siru Zhou <i@zhousiru.com>
2025-04-02 00:57:05 +0800

    feat: parse to struct

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
-use parser::{DirectiveParser, Rule};
-use pest::Parser;
+use parser::parse_hosts;
 
 mod parser;
 
 fn main() {
-  let parsed = DirectiveParser::parse(
-    Rule::directive,
-    "# OPTI-HOSTS example.com [Beijing, HK * 2, AS6939]",
-  )
-  .unwrap();
-  println!("{:#?}", parsed)
+  let result = parse_hosts(
+    r#"192.168.1.1 router.home
+127.0.0.1 localhost
+# OPTI-HOSTS example.com [Beijing, HK * 2, AS6939]
+# OPTI-HOSTS rua.sh [Alibaba * 2]
+127.0.0.2 notlocalhost
+# invalid below
+# OPTI-HOSTS rua.sh [Tencent * 0]"#,
+  );
+  println!("{:#?}", result)
 }
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,5 +1,64 @@
+use std::vec;
+
+use pest::Parser;
 use pest_derive::Parser;
 
 #[derive(Parser)]
 #[grammar = "parser/definition.pest"]
 pub struct DirectiveParser;
+
+#[derive(Debug, Clone)]
+pub struct LocationParam {
+  pub location: String,
+  pub limit: usize,
+}
+
+#[derive(Debug, Clone)]
+pub struct DirectiveMeta {
+  pub line_index: usize,
+  pub domain: String,
+  pub location_params: Vec<LocationParam>,
+}
+
+pub fn parse_hosts(hosts: &str) -> Vec<DirectiveMeta> {
+  let mut directive_metas: Vec<DirectiveMeta> = vec![];
+
+  for (line_index, line) in hosts.lines().enumerate() {
+    if !line.starts_with("#") {
+      continue;
+    }
+
+    match DirectiveParser::parse(Rule::directive, line) {
+      Ok(mut parsed) => {
+        let directive = parsed.next().unwrap();
+        let mut expression_inner = directive.into_inner().nth(1).unwrap().into_inner();
+        let domain_value = String::from(expression_inner.next().unwrap().as_str());
+        let array_inner = expression_inner.next().unwrap().into_inner();
+
+        let mut location_params: Vec<LocationParam> = vec![];
+
+        for item in array_inner {
+          let mut item_inner = item.into_inner();
+          let location_value = String::from(item_inner.next().unwrap().as_str());
+          let limit_value: usize = item_inner
+            .next()
+            .and_then(|v| v.as_str().parse().ok())
+            .unwrap_or(1);
+          location_params.push(LocationParam {
+            location: location_value,
+            limit: limit_value,
+          });
+        }
+
+        directive_metas.push(DirectiveMeta {
+          line_index,
+          domain: domain_value,
+          location_params,
+        });
+      }
+      Err(_) => (),
+    }
+  }
+
+  return directive_metas;
+}
